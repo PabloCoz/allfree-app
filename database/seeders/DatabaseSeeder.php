03f76f254f769bd46bb826386079975fde7bf9cf
PabloCoz <pablocr_234@hotmail.com>
2022-10-10 23:47:35 -0500

    actualiacion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,8 +29,8 @@ class DatabaseSeeder extends Seeder
         
         /* $this->call(PermissionSeeder::class);
         $this->call(RoleSeeder::class); */
-        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(LabelSeeder::class);
         $this->call(PostSeeder::class);
     }
